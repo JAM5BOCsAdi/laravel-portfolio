97ec7102128daf79f2cd474dda0d615124621378
JAM5BOCsAdi <oradam86@gmail.com>
2023-11-06 16:37:39 +0100

    Navbar | Footer | Changes | Next: Sections

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <script src="https://apis.google.com/js/platform.js"></script>
+    {{-- <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> --}}
+
+    <!-- AlpineJS -->
+    <!-- https://alpinejs.dev/start-here -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <!-- Tailwind css -->
+    <!-- https://tailwindcss.com/docs/guides/laravel -->
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="antialiased text-gray-800 dark:text-gray-200">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900 pt-24">
+        <x-layout.navbar></x-layout.navbar>
+
+        {{ $slot }}
+
+        <x-layout.footer></x-layout.footer>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/layout/navbar-item.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    <a href="{{ $href }}"
+        class="text-xs font-medium text-dark py-2 flex hover:text-primary dark:text-gray-200 lg:inline-flex lg:ml-6 xl:ml-12">{{ $slot }}</a>
+</div>
